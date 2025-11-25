aa17d0753d33de811cf6855f6ae90ec7953bc980
Viktek112 <Viktek229@mail.ru>
2025-11-25 22:43:53 +0300

    Commit of Linux only version

--- load.c
+++ load.c
@@ -2,26 +2,6 @@
 #include "load.h"
 #include "findMin.h"
 #include <stdio.h>
-#ifdef _WIN32
-#include <windows.h>
-void LoadRun(const char* const s, const int* const p, int size) {
-	void* lib;
-	void (*findMin)(const int* const p, int size);
-	lib = LoadLibrary(s); //загрузка библиотеки в память;
-	if (!lib) {
-		printf("cannot open library '%s'\n", s); 
-		return;
-	}
-	findMin = (void (*)(const int* const p, int size))GetProcAddress((HINSTANCE)lib, "findMin"); //получение указателя на функцию из библиотеки;
-	if (findMin == NULL) {
-		printf("cannot load function func\n");
-	}
-	else {
-		findMin(p, size);
-	}
-	FreeLibrary((HINSTANCE)lib); //выгрузка библиотеки;
-}
-#else
 #include <dlfcn.h>
 void LoadRun(const char* const s, const int* const p, int size) {
 	void* lib;
@@ -40,6 +20,5 @@ void LoadRun(const char* const s, const int* const p, int size) {
 	}
 	dlclose(lib); //выгрузка библиотеки;
 }
-#endif
 
 
