ec78bec570511f02ef62e4edcffe27cc993b3419
Viktek112 <Viktek229@mail.ru>
2025-11-25 22:30:27 +0300

    Maybe this time I will fix Windows branch

--- Source.c
+++ Source.c
@@ -41,8 +41,6 @@ int* fillArray(int size) {
     return array;
 }
 
-#ifdef _WIN32
-
 int main() {
     srand(time(NULL));
     int* A = fillArray(35);
@@ -64,30 +62,3 @@ int main() {
     getch();
     return 0;	// Завершаем программу с кодом успеха 0
 }
-
-#else // Linux
-
-int main() {
-    srand(time(NULL));
-    int* A = fillArray(35);
-    int* B = fillMatrix(8, 5);
-    print_matrix(B, 8, 5);
-    print_array(A, 35);
-    int input;
-    int result = -1;
-    do {
-        if (result == 0) {
-            char c;
-            while ((c = getchar()) != '\n' && c != EOF) {}
-        }
-        printf("I will work with matrix(1)/array(2)/exit(0): ");
-    } while (!(result = scanf("%d", &input)) || input < 0 || input > 2);
-
-    if (input == 1) LoadRun("matrixLib.so", B, 8 * 5);
-    else if (input == 2) LoadRun("arrayLib.so", A, 35);
-    getch();
-    return 0;	// Завершаем программу с кодом успеха 0
-}
-
-#endif
-
